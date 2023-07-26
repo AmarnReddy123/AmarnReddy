16ed5c96e0be081ba71c7e81dc9730a9664566de
AmarnReddy123 <140586378+AmarnReddy123@users.noreply.github.com>
2023-07-26 12:29:31 +0530

    Add files via upload

---
+++ ExtentReports/src/main/java/amarnathreddy/ExtentReports/App.java
@@ -0,0 +1,13 @@
+package amarnathreddy.ExtentReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
